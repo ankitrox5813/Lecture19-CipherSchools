6328e3a9921f97e3655bb9ea88920f68e841e3cb
Ankit Kumar Singh <35650455+ankitrox5813@users.noreply.github.com>
2023-07-15 00:29:35 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import ToDoScreen from "./screens/ToDoScreen";
+
+class App extends Component {
+    render(){
+        return <ToDoScreen/>;
+    }
+}
+
+export default App;

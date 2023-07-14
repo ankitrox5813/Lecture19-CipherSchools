f686c199c9702330957085a90b8e318b1fbb7e59
Ankit Kumar Singh <35650455+ankitrox5813@users.noreply.github.com>
2023-07-15 00:34:02 +0530

    Update ToDoAppScreen.js

--- ToDoAppScreen.js
+++ ToDoAppScreen.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 
 class App extends Component {
     render() {
-        return <h1>Welcome to ToDo List App</h1>;
+        return <h1 className="ui heading center">Welcome to ToDo List App</h1>;
     }
 }
 
